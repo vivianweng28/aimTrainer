59bb435b64123e48b340fb4d0ed6b2d0c732d734
Vivian <vweng01@students.cs.ubc.ca>
2023-10-26 12:40:52 -0700

    phase 2 code coverage fix, still need comments and citations!

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -59,9 +59,9 @@ class JsonWriterTest extends JsonTest {
     void testWriterGeneralSessionsAddNewSession() {
         try {
             Session s = new CircleSession(2);
-            s.setTarget(new CircleTarget(49,49));
+            s.setTarget(new CircleTarget(8,8));
             Suggestion first = new Suggestion(10, 10, "right", "up", 2, 2);
-            Suggestion second = new Suggestion(11, 11, "right", "up", 1, 1);
+            Suggestion second = new Suggestion(9, 9, "right", "up", 1, 1);
             s.addSuggestion(first);
             s.addSuggestion(second);
 
@@ -80,10 +80,10 @@ class JsonWriterTest extends JsonTest {
             checkSuggestion(50,50,"right", "up", 1, 1, s1Suggestions.get(0));
             checkSuggestion(49,49,"perfect", "perfect", 0, 0, s1Suggestions.get(1));
 
-            checkSessionNumTypeDistanceAndTarget(2, "circle",100, 49, 49, sessions.get(1));
+            checkSessionNumTypeDistanceAndTarget(2, "circle",100, 8, 8, sessions.get(1));
             List <Suggestion> s2Suggestions = sessions.get(1).getAllSuggestions();
             checkSuggestion(10, 10, "right", "up", 2, 2, s2Suggestions.get(0));
-            checkSuggestion(11, 11, "right", "up", 1, 1, s2Suggestions.get(1));
+            checkSuggestion(9, 9, "right", "up", 1, 1, s2Suggestions.get(1));
 
         } catch (IOException e) {
             fail("Exception should not have been thrown");
@@ -98,12 +98,10 @@ class JsonWriterTest extends JsonTest {
             Session s = sessions.get(0);
 
             s.addOldSessionsToPastSessions(reader);
-            Suggestion first = new Suggestion(10, 10, "right", "up", 2, 2);
-            Suggestion second = new Suggestion(11, 11, "right", "up", 1, 1);
+            Suggestion first = new Suggestion(60, 60, "right", "up", 11, 11);
+            Suggestion second = new Suggestion(59, 59, "right", "up", 10, 10);
             s.addSuggestion(first);
             s.addSuggestion(second);
-            Suggestion origFirst = new Suggestion(50,50,"right", "up", 1, 1);
-            Suggestion origSecond = new Suggestion(51,51,"perfect", "perfect", 0, 0);
 
             JsonWriter writer = new JsonWriter("./data/testWriterGeneralSessionReplace.json");
             writer.open();
@@ -116,8 +114,39 @@ class JsonWriterTest extends JsonTest {
             List <Suggestion> s1Suggestions = sessions.get(0).getAllSuggestions();
             checkSuggestion(50,50,"right", "up", 1, 1, s1Suggestions.get(0));
             checkSuggestion(49,49,"perfect", "perfect", 0, 0, s1Suggestions.get(1));
-            checkSuggestion(10, 10, "right", "up", 2, 2, s1Suggestions.get(2));
-            checkSuggestion(11, 11, "right", "up", 1, 1, s1Suggestions.get(3));
+            checkSuggestion(60, 60, "right", "up", 11, 11, s1Suggestions.get(2));
+            checkSuggestion(59, 59, "right", "up", 10, 10, s1Suggestions.get(3));
+
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterPastSessionNull() {
+        try {
+            JsonReader reader = new JsonReader("./data/testWriterEmptyPastSessions.json");
+            sessions = reader.read();
+            Session s = new CircleSession(1);
+            s.setTarget(new CircleTarget(8, 8));
+
+            s.addOldSessionsToPastSessions(reader);
+            Suggestion first = new Suggestion(10, 10, "right", "up", 2, 2);
+            Suggestion second = new Suggestion(9, 9, "right", "up", 1, 1);
+            s.addSuggestion(first);
+            s.addSuggestion(second);
+
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyPastSessions.json");
+            writer.open();
+            writer.write(s);
+            writer.close();
+
+            sessions = reader.read();
+            assertEquals(1, sessions.size());
+            checkSessionNumTypeDistanceAndTarget(1, "circle", 100, 8,8, sessions.get(0));
+            List <Suggestion> s1Suggestions = sessions.get(0).getAllSuggestions();
+            checkSuggestion(10, 10, "right", "up", 2, 2, s1Suggestions.get(0));
+            checkSuggestion(9, 9, "right", "up", 1, 1, s1Suggestions.get(1));
 
         } catch (IOException e) {
             fail("Exception should not have been thrown");
